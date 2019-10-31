92b5f6d50fc19f29f71ffd7437086693c6518b47
kirya <kyrylo.dobrynin@gmail.com>
2019-10-31 17:54:19 +0200

    v.2

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ enable_cool_new_feature = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 
 
 @application.route('/')
 def hello_world():
-    message = "Hello, Kirya!"
+    message = "Hello, World!!!"
     return flask.render_template('index.html',
                                   title=message,
                                   flask_debug=application.debug,
